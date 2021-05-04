08c514cb976607d06f310f06b9128fffdc753191
Štěpán Tomsa <stomsa@redhat.com>
2021-05-04 13:19:47 +0200

    Make spec tester a class

--- tests/test_translating_parser.py
+++ tests/test_translating_parser.py
@@ -5,34 +5,37 @@ from pytest import mark
 from translating_parser.parser import TranslatingParser
 
 
-def _parse_spec(file):
-    path = join("tests", "specs", file)
-    parser = TranslatingParser(path)
-    parser.parse()
-    return parser.specification
+class SpecificationTester:
+    @staticmethod
+    def _parse_spec(file):
+        path = join("tests", "specs", file)
+        parser = TranslatingParser(path)
+        parser.parse()
+        return parser.specification
 
+    def __init__(self, file):
+        self.specification = self._parse_spec(file)
 
-def _assert_path_ref(specification, ref):
-    responses = specification["paths"]["/hosts"]["get"]["responses"]
-    expected_schema = responses["default"]["content"]["application/json"]["schema"]
-    assert "$ref" in expected_schema
-    assert expected_schema["$ref"] == ref
+    def assert_path_ref(self, ref):
+        responses = self.specification["paths"]["/hosts"]["get"]["responses"]
+        schema = responses["default"]["content"]["application/json"]["schema"]
+        assert "$ref" in schema
+        assert schema["$ref"] == ref
 
-
-def _assert_schemas(specification, keys):
-    assert specification["components"]["schemas"].keys() == keys
+    def assert_schemas(self, keys):
+        assert self.specification["components"]["schemas"].keys() == keys
 
 
 def test_local_reference_from_root():
-    specification = _parse_spec("local_ref.spec.yaml")
-    _assert_path_ref(specification, "#/components/schemas/PlainObject")
-    _assert_schemas(specification, {"PlainObject"})
+    tester = SpecificationTester("local_ref.spec.yaml")
+    tester.assert_path_ref("#/components/schemas/PlainObject")
+    tester.assert_schemas({"PlainObject"})
 
 
 def test_file_reference_from_root():
-    specification = _parse_spec("file_ref.spec.yaml")
-    _assert_path_ref(specification, "#/components/schemas/plain_obj.spec.yaml_PlainObject")
-    _assert_schemas(specification, {"plain_obj.spec.yaml_PlainObject"})
+    tester = SpecificationTester("file_ref.spec.yaml")
+    tester.assert_path_ref("#/components/schemas/plain_obj.spec.yaml_PlainObject")
+    tester.assert_schemas({"plain_obj.spec.yaml_PlainObject"})
 
 
 @mark.skip
