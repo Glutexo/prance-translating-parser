649eec056968ab86aa8df11e8f552dbed4f6fd34
Štěpán Tomsa <stomsa@redhat.com>
2021-05-04 12:55:59 +0200

    DRY spec parsing in tests

--- tests/test_translating_parser.py
+++ tests/test_translating_parser.py
@@ -5,28 +5,29 @@ from pytest import mark
 from translating_parser.parser import TranslatingParser
 
 
-def test_local_reference_from_root():
-    path = join("tests", "specs", "local_ref.spec.yaml")
+def _parse_spec(file):
+    path = join("tests", "specs", file)
     parser = TranslatingParser(path)
     parser.parse()
+    return parser.specification
 
-    parsed_responses = parser.specification["paths"]["/hosts"]["get"]["responses"]
+
+def test_local_reference_from_root():
+    specification = _parse_spec("local_ref.spec.yaml")
+    parsed_responses = specification["paths"]["/hosts"]["get"]["responses"]
     expected_schema = parsed_responses["default"]["content"]["application/json"]["schema"]
     assert "$ref" in expected_schema
     assert expected_schema["$ref"] == "#/components/schemas/PlainObject"
-    assert parser.specification["components"]["schemas"].keys() == {"PlainObject"}
+    assert specification["components"]["schemas"].keys() == {"PlainObject"}
 
 
 def test_file_reference_from_root():
-    path = join("tests", "specs", "file_ref.spec.yaml")
-    parser = TranslatingParser(path)
-    parser.parse()
-
-    parsed_responses = parser.specification["paths"]["/hosts"]["get"]["responses"]
+    specification = _parse_spec("file_ref.spec.yaml")
+    parsed_responses = specification["paths"]["/hosts"]["get"]["responses"]
     expected_schema = parsed_responses["default"]["content"]["application/json"]["schema"]
     assert "$ref" in expected_schema
     assert expected_schema["$ref"] == "#/components/schemas/plain_obj.spec.yaml_PlainObject"
-    assert parser.specification["components"]["schemas"].keys() == {"plain_obj.spec.yaml_PlainObject"}
+    assert specification["components"]["schemas"].keys() == {"plain_obj.spec.yaml_PlainObject"}
 
 
 @mark.skip
